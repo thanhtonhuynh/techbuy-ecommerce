56b32edc69908b1ab584aa38206afac743dbf46f
Ton Huynh <kintonhtt@gmail.com>
2024-11-22 17:33:02 -0800

    fix tw indicator style

--- src/components/TailwindScreenSizeIndicator.tsx
+++ src/components/TailwindScreenSizeIndicator.tsx
@@ -2,13 +2,13 @@ export function TailwindScreenSizeIndicator() {
   if (process.env.NODE_ENV === "production") return null;
 
   return (
-    <div className="fixed bottom-1 right-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-muted p-3 font-mono text-xs font-medium text-destructive">
-      <div className="block sm:hidden">xs</div>
-      <div className="hidden sm:block md:hidden">sm</div>
-      <div className="hidden md:block lg:hidden">md</div>
-      <div className="hidden lg:block xl:hidden">lg</div>
-      <div className="hidden xl:block 2xl:hidden">xl</div>
-      <div className="hidden 2xl:block">2xl</div>
+    <div className="fixed bottom-4 right-4 z-50 flex h-7 w-7 items-center justify-center rounded-full bg-white p-3 font-mono text-xs font-bold text-muted-foreground shadow-[0_0_10px_0_rgba(0,0,0,0.3)]">
+      <div className="block sm:hidden">XS</div>
+      <div className="hidden sm:block md:hidden">SM</div>
+      <div className="hidden md:block lg:hidden">MD</div>
+      <div className="hidden lg:block xl:hidden">LG</div>
+      <div className="hidden xl:block 2xl:hidden">XL</div>
+      <div className="hidden 2xl:block">2XL</div>
     </div>
   );
 }
