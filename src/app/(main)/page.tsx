02c4c31ed7b714986d216aa7548e19c780a4f765
Ton Huynh <kintonhtt@gmail.com>
2024-11-22 17:22:22 -0800

    fix hompage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,13 +7,19 @@ export default async function Home() {
 
   return (
     <>
-      <section className="flex items-center justify-center gap-8 xl:gap-32 px-4 py-8 md:px-8 md:py-10 lg:py-12">
-        <div className="relative hidden h-72 items-center justify-center lg:flex">
-          <div className="animate-blobby absolute -left-16 h-72 w-72 rounded-full bg-blue-300 mix-blend-multiply blur-2xl filter"></div>
-          <div className="animate-blobby delay-2000 absolute -right-16 h-72 w-72 rounded-full bg-orange-300 mix-blend-multiply blur-2xl filter"></div>
-          <div className="animate-blobby delay-4000 absolute -bottom-10 left-20 h-72 w-72 rounded-full bg-pink-300 mix-blend-multiply blur-2xl filter"></div>
-          <h1>Personal. Private. Powerful.</h1>
+      <section className="mb-8 flex items-center justify-center gap-8 px-4 py-8 md:py-10 lg:gap-16 lg:py-12 xl:gap-32">
+        <div className="relative hidden h-72 w-fit items-center justify-center lg:flex">
+          <div className="absolute -left-16 h-72 w-2/3 animate-blobby rounded-full bg-blue-300 mix-blend-multiply blur-2xl filter"></div>
+          <div className="absolute -right-16 h-72 w-1/2 animate-blobby rounded-full bg-orange-300 mix-blend-multiply blur-2xl filter delay-2000"></div>
+          <div className="absolute -bottom-10 left-20 h-72 w-2/3 animate-blobby rounded-full bg-pink-300 mix-blend-multiply blur-2xl filter delay-4000"></div>
+          <h1 className="flex flex-row gap-2 text-4xl">
+            <span>Personal.</span> <span>Private.</span> <span>Powerful.</span>
+          </h1>
         </div>
+
+        <h1 className="gradient-text flex flex-col text-4xl lg:hidden">
+          <span>Personal.</span> <span>Private.</span> <span>Powerful.</span>
+        </h1>
 
         <Image
           src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-imac-202410?wid=500&hei=348&fmt=p-jpg&qlt=100&.v=1731974953703"
@@ -22,6 +28,11 @@ export default async function Home() {
           height={348}
           quality={100}
         />
+      </section>
+
+      <section className="space-y-2 px-4 md:px-16">
+        <h1 className="gradient-text w-fit">Popular Products</h1>
+        <p className="text-muted-foreground">See what others are buying.</p>
       </section>
 
       <ProductsCarousel products={products} />
