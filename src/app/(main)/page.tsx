04bbfb9c4108a84e7e980c08e98ecf96d399bbbb
Ton Huynh <kintonhtt@gmail.com>
2024-12-06 00:38:00 -0800

    add latest products section to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,15 +3,21 @@ import { getProducts } from "@/data-access/product";
 import Image from "next/image";
 
 export default async function Home() {
-  const { products } = await getProducts({
+  const { products: popularProducts } = await getProducts({
     status: "active",
     sortKey: "purchasedCount",
     reverse: true,
   });
 
+  const { products: latestProducts } = await getProducts({
+    status: "active",
+    sortKey: "createdAt",
+    reverse: true,
+  });
+
   return (
     <>
-      <section className="mb-8 flex items-center justify-center gap-8 px-4 py-8 md:py-10 lg:gap-16 lg:py-12 xl:gap-32">
+      <section className="flex items-center justify-center gap-8 px-4 py-8 md:py-10 lg:gap-16 lg:py-12 xl:gap-32">
         <div className="relative hidden h-72 w-fit items-center justify-center lg:flex">
           <div className="absolute -left-16 h-72 w-2/3 animate-blobby rounded-full bg-blue-300 mix-blend-multiply blur-2xl filter"></div>
           <div className="absolute -right-16 h-72 w-1/2 animate-blobby rounded-full bg-orange-300 mix-blend-multiply blur-2xl filter delay-2000"></div>
@@ -34,12 +40,19 @@ export default async function Home() {
         />
       </section>
 
-      <section className="space-y-2 px-4 md:px-16">
+      <section className="mt-16 space-y-2 px-4 pt-4 md:px-16">
         <h1 className="gradient-text w-fit">Popular Products</h1>
-        <p className="text-muted-foreground">See what others are buying.</p>
+        <p className="text-muted-foreground">Discover the most popular products in our store.</p>
       </section>
 
-      <ProductsCarousel products={products} />
+      <ProductsCarousel products={popularProducts} />
+
+      <section className="mt-8 space-y-2 px-4 md:px-16">
+        <h1 className="gradient-text w-fit">Latest Products</h1>
+        <p className="text-muted-foreground">New. Now. Next. See what's new in our store.</p>
+      </section>
+
+      <ProductsCarousel products={latestProducts.slice(0, 10)} />
     </>
   );
 }
